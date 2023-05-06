b8cecf580c3fc7ef01297fd181307a2a08d1575c
CrazyCatZhang <CrazyCatZhang@outlook.com>
2023-05-07 01:16:00 +0800

    React combined with StoryBook

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ChatApp from "components/ChatApp";
+import {ThemeProvider} from "styled-components";
+import theme from "theme";
+import {BrowserRouter as Router} from "react-router-dom";
+
+function App() {
+    return (
+        <div>
+            App
+        </div>
+    );
+}
+
+export default App;
